0b40db6dc489d796baf362ba1ad97cb4c63c3bd5
Lizaveta Niamera <lniamera@cloudferro.com>
2022-03-30 08:59:24 +0200

    added for urls list

--- source/get_rst_files.py
+++ source/get_rst_files.py
@@ -22,26 +22,33 @@ def check_updates(github_file_content: str, local_file_path: str) -> bool:
     return True
 
 
-def get_files(url: str) -> None:
-    repo_name, dir_name = process_url(url)
-    if repo_name is None:
-        return
-    gh = Github()
-    repo = gh.get_repo(repo_name)
-    github_raw_url = "https://raw.githubusercontent.com"
-    contents_list = repo.get_contents(dir_name)
-    for content in contents_list:
-        if content.type == "file":
-            github_file_url = (
-                github_raw_url
-                + "/"
-                + repo.full_name
-                + "/master/"
-                + dir_name
-                + "/"
-                + content.path.replace(" ", "%20").split("/")[-1]
-            )
-            github_file = requests.get(github_file_url)
-            if check_updates(github_file.content, content.path.split("/")[-1]):
-                urlretrieve(github_file_url, content.path.split("/")[-1])
+def get_files(urls_list: list) -> None:
+    for url in urls_list:
+        repo_name, dir_name = process_url(url)
+        if repo_name is None:
+            return
+        gh = Github()
+        repo = gh.get_repo(repo_name)
+        github_raw_url = "https://raw.githubusercontent.com"
+        contents_list = repo.get_contents(dir_name)
+        for content in contents_list:
+            if content.type == "file":
+                github_file_url = (
+                    github_raw_url
+                    + "/"
+                    + repo.full_name
+                    + "/master/"
+                    + dir_name
+                    + "/"
+                    + content.path.replace(" ", "%20").split("/")[-1]
+                )
+                github_file = requests.get(github_file_url)
+                if check_updates(github_file.content, content.path.split("/")[-1]):
+                    urlretrieve(github_file_url, content.path.split("/")[-1])
+
+get_files(
+    [
+        "https://github.com/CloudFerro/doc/blob/master/source/Gstarted",
+    ]
+)
 
