bb640cd7b8f55d82624baeec66373599d900b762
Lizaveta Niamera <lniamera@cloudferro.com>
2022-04-01 17:31:25 +0200

    added overriden edit on github url

--- source/conf.py
+++ source/conf.py
@@ -17,9 +17,9 @@ from get_rst_files import get_files
 
 # -- Project information -----------------------------------------------------
 
-project = 'mydocs'
-copyright = '2022, Liza'
-author = 'Liza'
+project = "mydocs"
+copyright = "2022, Liza"
+author = "Liza"
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,11 +27,10 @@ author = 'Liza'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -44,11 +43,18 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
+
+urls_dict = get_files('https://github.com/CloudFerro/doc/tree/master/source/Gstarted')
+
+
+html_context = {
+    "display_github": True,
+    "urls_dict": urls_dict,
+    "test_var": "123 test var 123",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-get_files('https://github.com/CloudFerro/doc/tree/master/source/Gstarted')
+html_static_path = ["_static"]
--- source/get_rst_files.py
+++ source/get_rst_files.py
@@ -22,33 +22,22 @@ def check_updates(github_file_content: str, local_file_path: str) -> bool:
     return True
 
 
-def get_files(urls_list: list) -> None:
+def get_files(urls_list: list) -> dict:
+    repos_url = {}
     for url in urls_list:
         repo_name, dir_name = process_url(url.replace("blob", "tree"))
         if repo_name is None:
             return
         gh = Github()
         repo = gh.get_repo(repo_name)
-        github_raw_url = "https://raw.githubusercontent.com"
         contents_list = repo.get_contents(dir_name)
         for content in contents_list:
             if content.type == "file":
-                github_file_url = (
-                    github_raw_url
-                    + "/"
-                    + repo.full_name
-                    + "/master/"
-                    + dir_name
-                    + "/"
-                    + content.path.replace(" ", "%20").split("/")[-1]
-                )
+                if ".rst" in content.name:
+                    repos_url[content.name.split('.')[0]] = content.html_url
+                github_file_url = content.download_url
                 github_file = requests.get(github_file_url)
                 if check_updates(github_file.content, content.path.split("/")[-1]):
                     urlretrieve(github_file_url, content.path.split("/")[-1])
-
-get_files(
-    [
-        "https://github.com/CloudFerro/doc/blob/master/source/Gstarted",
-    ]
-)
-
+    return repos_url
+    
