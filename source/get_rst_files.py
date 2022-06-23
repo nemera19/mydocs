609bb8c7e199e4934f6d51cf1e6c95b022853596
Lizaveta Niamera <lniamera@cloudferro.com>
2022-06-23 06:59:22 +0200

    added try exception

--- source/get_rst_files.py
+++ source/get_rst_files.py
@@ -1,33 +1,22 @@
-import os
 import re
+import os
 from os.path import exists
 from urllib.request import urlretrieve
-import time
+
 import requests
 import github
-from multiprocessing.dummy import Pool
 from github import Github
 
 from sphinx.util import logging
 
 
-from conf import brand_name
-
 logger = logging.getLogger(__name__)
-
 
 def process_url(url: str):
     if "tree" in url:
         repo, dir_name = re.split(r"/tree/\w+/", url)
         return repo.replace("https://github.com/", ""), dir_name
     return None, None
-
-
-def process_content(content):
-    content_path = content.path
-    if "|brand-name|" in content.name:
-        content_path = content.path.replace("|brand-name|", brand_name)
-    return content_path.split("/")[-1]
 
 
 def urls_list_to_dict(urls_list: list) -> dict:
@@ -50,56 +39,37 @@ def check_updates(github_file_content: str, local_file_path: str) -> bool:
         os.makedirs(dir_name)
     return True
 
-def download_file(github_file_url, file_path):
-    urlretrieve(
-                github_file_url,
-                file_path,
-            )
-
-
-def download_content(content, folder):
-    content_url = {}
-    content_name = process_content(content)
-    if content.type == "file":
-        if ".rst" in content.name:
-            content_url[
-                folder + "/" + content_name.replace(".rst", "")
-            ] = content.html_url
-        github_file_url = content.download_url
-        github_file = requests.get(github_file_url)
-        if check_updates(
-            github_file.content,
-            folder + "/" + content_name,
-        ):
-            download_file(github_file_url, folder + "/" + content_name)
-    return content_url
-
-
-def get_directory_content(folder: str, urls_list: dict):
-    urls_dict = urls_list_to_dict(urls_list.get(folder))
-    gh = Github(login_or_token=os.environ.get("github_token", ""))
-    for repo, dirs in urls_dict.items():
-        repo = gh.get_repo(repo)
-        for dir_name in dirs:
-            contents_list = []
-            try:
-                contents_list = repo.get_contents(dir_name)
-            except github.GithubException as e:
-                logger.warning(
-                   'directory "%s" was omitted, "%s", "%s"'
-                    % (dir_name, e.status, e.data["message"])
-                )
-            content_urls = {}
-            for content in contents_list:
-                content_urls.update(download_content(content, dir_name))
-    return content_urls
-                
-
 
 def get_files(urls_list: dict) -> dict:
     external_repos_url = {}
-    pool = Pool(4)
-    get_directory_content(urls_list.keys())
-    external_repos_url.update(pool.map(get_directory_content, urls_list.keys()))
-    print(external_repos_url)
+    for folder in urls_list.keys():
+        urls_dict = urls_list_to_dict(urls_list.get(folder))
+        gh = Github(login_or_token=os.environ.get("github_token", ""))
+        for repo, dirs in urls_dict.items():
+            repo = gh.get_repo(repo)
+            for dir_name in dirs:
+                contents_list = []
+                try:
+                    contents_list = repo.get_contents(dir_name)
+                except github.GithubException as e:
+                    logger.warning(
+                        '"%s" is omitted, "%s", "%s"'
+                        % (dir_name, e.status, e.data["message"])
+                    )
+                for content in contents_list:
+                    if content.type == "file":
+                        if ".rst" in content.name:
+                            external_repos_url[
+                                folder + "/" + content.name.replace(".rst", "")
+                            ] = content.html_url
+                        github_file_url = content.download_url
+                        github_file = requests.get(github_file_url)
+                        if check_updates(
+                            github_file.content,
+                            folder + "/" + content.path.split("/")[-1],
+                        ):
+                            urlretrieve(
+                                github_file_url,
+                                folder + "/" + content.path.split("/")[-1],
+                            )
     return external_repos_url
